aaa4cd6bfb22c2148ca611026ca771a8975faa1f
Omar Baradei <omarbaradei21@gmail.com>
2019-01-29 23:04:46 -0500

    Connected to firebase

--- app/src/main/java/com/example/mytestapplication/MainActivity.java
+++ app/src/main/java/com/example/mytestapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mytestapplication;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +15,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +43,31 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "AYEEEEEE WE IN HERE BOI", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                // Write a message to the database
+                // Create a new user with a first and last name
+                Map<String, Object> user = new HashMap<>();
+                user.put("username", "Ada");
+                user.put("password", "Lovelace");
+                user.put("born", 1815);
+
+                // Add a new document with a generated ID
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+
             }
         });
 
